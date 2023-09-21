0410ec13513329fe68b50e25768fdbb807581b1c
JesuLopezGomez <jelopez.gom@gmail.com>
2023-09-21 13:25:14 +0200

    actualizacion

--- src/dbconnect/main/java/MainApp.java
+++ src/dbconnect/main/java/MainApp.java
@@ -8,80 +8,119 @@ import dbconnect.main.java.api.Connector;
 public class MainApp {
 
 		public static void main(String[] args) {
-			Connector c = new Connector();
-			
-			final String MENU ="--------------------------------------------------------------------------"
-			+ "\n 1. Mostrar información sobre los clientes"
-			+ "\n 2. Mostrar pedidos"
-			+ "\n 3. Añadir cliente"
-			+ "\n 4. Actualizar un cliente existente"
-			+ "\n 5. Eliminar cliente"
-			+ "\n 6. Añadir Pedido"
-			+ "\n 7. Incluir Líneas"
-			+ "\n 8. Salir"
-			+"\n -------------------------------------------------------------------------";
-			
 			Scanner sc = new Scanner(System.in);
-
+			Connector c = new Connector();
 			int opcion = 0;
+			
+			final String MENU =" ---------------------------------------------"
+			+ "\n | 1. Mostrar información sobre los clientes |"
+			+ "\n | 2. Mostrar pedidos                        |"
+			+ "\n | 3. Añadir cliente                         |"
+			+ "\n | 4. Actualizar un cliente existente        |"
+			+ "\n | 5. Eliminar cliente                       |"
+			+ "\n | 6. Añadir Pedido                          |"
+			+ "\n | 7. Incluir Líneas                         |"
+			+ "\n | 8. Salir                                  |"
+			+"\n ----------------------------------------------";
+			
+			do { 
+				do {
+					System.out.println(MENU);
+					opcion = Integer.valueOf(sc.nextLine());
+				} while(opcion!=1 && opcion!=2 && opcion!=3 && opcion!=4 && opcion!=5 && opcion!=6 && opcion!=7 && opcion!=8);
+				if (opcion==1) {
+					try {
+						System.out.println(c.mostrarInfoCliente());
+					} catch (ClassNotFoundException  | SQLException e) {
+						e.printStackTrace();
+					} 
+				}else if (opcion==2) {
+					try {
+						System.out.println(c.mostrarInfoPedidos());
+					} catch (SQLException e) {
+						e.printStackTrace();
+					} 
+				}else if (opcion==3) {
+					System.out.println("Introduce el nombre:");
+					String nombre = sc.nextLine();
+					System.out.println("Introduce el apellido:");
+					String apellido = sc.nextLine();
+					System.out.println("Introduce el email: ");
+					String email = sc.nextLine();
+					System.out.println("Introduce el año de nacimiento: ");
+					String annio = sc.nextLine();
+					System.out.println("Introduce el mes de nacimiento: ");
+					String mes = sc.nextLine();
+					System.out.println("Introduce el dia de nacimiento: ");
+					String dia = sc.nextLine();
+					System.out.println("Introduce el sexo M/F");
+					String sexo = sc.nextLine();
+					try {
+						c.anniadirCliente(nombre, apellido,email, annio+"-"+mes+"-"+dia+"", sexo);
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}else if (opcion==4) {
+					System.out.println("Introduce el id de la persona que quieres modificar: ");
+					int id = Integer.valueOf(sc.nextLine());
+					System.out.println("Introduce el nombre:");
+					String nombre = sc.nextLine();
+					System.out.println("Introduce el apellido:");
+					String apellido = sc.nextLine();
+					
+					try {
+						c.actualizarCliente(id, nombre, apellido);
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}else if (opcion==5) {
+					System.out.println("Introduce el id de la persona que quieres eliminar: ");
+					int id = Integer.valueOf(sc.nextLine());
+					
+					try {
+						c.eliminarCliente(id);
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
 
+				}else if (opcion==6) {
+					System.out.println("Introduce el id:");
+					int id = Integer.valueOf(sc.nextLine());
+					System.out.println("Introduce el codigo:");
+					String cod = sc.nextLine();
+					System.out.println("Introduce el status: ");
+					String status = sc.nextLine();
+					System.out.println("Introduce el idCliente: ");
+					int idCli = Integer.valueOf(sc.nextLine());
+					
+					try {
+						c.anniadirPedido(id, cod,status, idCli);
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
 
-			while (opcion!=8) {
+				}else if (opcion==7) {
+					System.out.println("Introduce el id:");
+					int id = Integer.valueOf(sc.nextLine());
+					System.out.println("Introduce el codigo:");
+					String codigo = sc.nextLine();
+					System.out.println("Introduce el nombre de producto: ");
+					String nombreProducto = sc.nextLine();
+					System.out.println("Introduce el id pedido: ");
+					int idPedido = Integer.valueOf(sc.nextLine());
+					System.out.println("Introduce la cantidad: ");
+					int cantidad = Integer.valueOf(sc.nextLine());
+					System.out.println("Introduce el precio: ");
+					int precio = Integer.valueOf(sc.nextLine());
+					try {
+						c.anniadirLinea(id, codigo, nombreProducto, idPedido, cantidad, precio);
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
 
-			System.out.println(MENU);
-
-			System.out.println("Introduzca la opcion que más le interese: ");
-
-			opcion = Integer.valueOf(sc.nextLine());
-
-			if (opcion==1) {
-				try {
-					System.out.println(c.mostrarInfoCliente());
-				} catch (ClassNotFoundException  | SQLException e) {
-					e.printStackTrace();
-				} 
-			}else if (opcion==2) {
-				try {
-					System.out.println(c.mostrarInfoPedidos());
-				} catch (SQLException e) {
-					e.printStackTrace();
-				} 
-			}else if (opcion==3) {
-				System.out.println("Introduce el nombre:");
-				String nombre = sc.nextLine();
-				System.out.println("Introduce el apellido:");
-				String apellido = sc.nextLine();
-				System.out.println("Introduce el email: ");
-				String email = sc.nextLine();
-				System.out.println("Introduce el año de nacimiento: ");
-				String annio = sc.nextLine();
-				System.out.println("Introduce el mes de nacimiento: ");
-				String mes = sc.nextLine();
-				System.out.println("Introduce el dia de nacimiento: ");
-				String dia = sc.nextLine();
-				System.out.println("Introduce el sexo M/F");
-				String sexo = sc.nextLine();
-				try {
-					c.anniadirCliente(nombre, apellido,email, annio+"-"+mes+"-"+dia+"", sexo);
-				} catch (SQLException e) {
-					e.printStackTrace();
 				}
-			}else if (opcion==4) {
-
-
-			}else if (opcion==5) {
-
-
-			}else if (opcion==6) {
-
-
-			}else if (opcion==7) {
-
-
-			}
-
-			}System.out.println("Acabando..");
-
+			} while(opcion!=8);
+			System.out.println("Programa finalizado.");
 	
 		}
 }
--- src/dbconnect/main/java/api/Connector.java
+++ src/dbconnect/main/java/api/Connector.java
@@ -9,29 +9,30 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import dbconnect.main.java.model.Cliente;
 
 public class Connector {
-	
-	
+
 	private static String USER;
-	private final static String DB_USER_KEY="db.user";
+	private final static String DB_USER_KEY = "db.user";
 
 	private static String PASS;
-	private final static String DB_PASS_KEY="db.pass";
-	
+	private final static String DB_PASS_KEY = "db.pass";
+
 	private static String JDBC_URL;
-	private final static String DB_URL_KEY="db.url";
-	
+	private final static String DB_URL_KEY = "db.url";
+
 	private final static String PROPERTIES_URI = "./resources/db.properties";
-	
+
 	public Connector() {
 		super();
 		loadProperties();
 	}
-	
+
 	public void loadProperties() {
 		try {
 			Properties properties = new Properties();
@@ -44,45 +45,46 @@ public class Connector {
 		}
 	}
 
-	
-	
 	public String mostrarInfoCliente() throws SQLException, ClassNotFoundException {
 		StringBuilder sb = new StringBuilder();
-		
-		Connection connection = conect(); 
+
+		Connection connection = conect();
 
 		Statement statement = statement(connection);
-		
-		ResultSet rs=statement.executeQuery("select * from Cliente");  
+
+		ResultSet rs = statement.executeQuery("select * from Cliente");
 		sb.append("nombre---apellidos---email---fechaNacimiento---genero").append(System.lineSeparator());
-		while(rs.next()) {//Avanza de posición en el listado de registros y devuelve true si existe tal
-			Cliente alumno = new Cliente(Integer.valueOf(rs.getString(1)), rs.getString(2), rs.getString(3), 
-										rs.getString(5), rs.getString(4));
-			sb.append(rs.getString(2)+"---"+rs.getString(3)+"---"+rs.getString(4)+"---"+(LocalDate.now().getYear()-Integer.valueOf(rs.getString(5).substring(0,4)))+"---"+rs.getString(6)).append(System.lineSeparator());  
+		while (rs.next()) {// Avanza de posición en el listado de registros y devuelve true si existe tal
+			Cliente alumno = new Cliente(Integer.valueOf(rs.getString(1)), rs.getString(2), rs.getString(3),
+					rs.getString(5), rs.getString(4));
+			sb.append(rs.getString(2) + "---" + rs.getString(3) + "---" + rs.getString(4) + "---"
+					+ (LocalDate.now().getYear() - Integer.valueOf(rs.getString(5).substring(0, 4))) + "---"
+					+ rs.getString(6)).append(System.lineSeparator());
 		}
-		connection.close();  
-		
+		connection.close();
+
 		return sb.toString();
 	}
-	
+
 	public String mostrarInfoPedidos() throws SQLException {
 		StringBuilder sb = new StringBuilder();
-		Connection connection = conect(); 
+		Connection connection = conect();
 
 		Statement statement = statement(connection);
-		
-		ResultSet rs=statement.executeQuery("SELECT P.id,P.status,C.nombre,C.apellido,L.cantidad,L.precio FROM Pedido P ,Linea L,Cliente C WHERE P.idCliente = C.id AND L.idPedido = P.id");  
-		sb.append("codigo---status---nombreCompletoCliente---numProducto---Importe").append(System.lineSeparator());
-		while(rs.next()) {
-			sb.append(String.format("%s---%s---%s %s---%s---%s",rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6))).append(System.lineSeparator());
-		}
-		
-		return sb.toString();
 
+		ResultSet rs = statement.executeQuery(
+				"SELECT P.id,P.status,C.nombre,C.apellido,L.cantidad,L.precio FROM Pedido P ,Linea L,Cliente C WHERE P.idCliente = C.id AND L.idPedido = P.id");
+		sb.append("codigo---status---nombreCompletoCliente---numProducto---Importe").append(System.lineSeparator());
+		while (rs.next()) {
+			sb.append(String.format("%s---%s---%s %s---%s---%s", rs.getString(1), rs.getString(2), rs.getString(3),
+					rs.getString(4), rs.getString(5), rs.getString(6))).append(System.lineSeparator());
+		}
+
+		return sb.toString();
 	}
 
 	private Statement statement(Connection connection) throws SQLException {
-		Statement statement=connection.createStatement();  
+		Statement statement = connection.createStatement();
 		return statement;
 	}
 
@@ -90,24 +92,93 @@ public class Connector {
 		Connection connection = DriverManager.getConnection(JDBC_URL, USER, PASS);
 		return connection;
 	}
-	
-	public void anniadirCliente(String nombre,String apellido,String email,String fechaNacimiento,String genero) throws SQLException {
-		Connection connection = conect(); 
+
+	public void anniadirCliente(String nombre, String apellido, String email, String fechaNacimiento, String genero)
+			throws SQLException {
+		Connection connection = conect();
 		Statement statement = statement(connection);
-		PreparedStatement ps = connection.prepareStatement("INSERT INTO Cliente (nombre, apellido, email, fechaNacimiento, genero) "
-			+ " VALUES (?, ?, ?, ?, ?)");
-			ps.setString(1, nombre);
-			ps.setString(2, apellido);
-			ps.setString(3, email);
-			ps.setString(4, fechaNacimiento);
-			ps.setString(5, genero);
+		PreparedStatement ps = connection.prepareStatement(
+				"INSERT INTO Cliente (nombre, apellido, email, fechaNacimiento, genero) " + " VALUES (?, ?, ?, ?, ?)");
+		ps.setString(1, nombre);
+		ps.setString(2, apellido);
+		ps.setString(3, email);
+		ps.setString(4, fechaNacimiento);
+		ps.setString(5, genero);
+		ps.executeUpdate();
+		ps.close();
+
+	}
+
+	public void actualizarCliente(int id, String nombre, String apellido) throws SQLException {
+		Connection connection = conect();
+		Statement statement = statement(connection);
+		PreparedStatement ps = connection
+				.prepareStatement("UPDATE Cliente c SET c.nombre = ?, c.apellido = ? WHERE c.id = ?");
+
+		ps.setString(1, nombre);
+		ps.setString(2, apellido);
+		ps.setInt(3, id);
+		ps.executeUpdate();
+		ps.close();
+
+	}
+
+	public void eliminarCliente(int id) throws SQLException {
+		Connection connection = conect();
+		Statement statement = statement(connection);
+
+		int idPedido = 0;
+
+		ResultSet rs = statement.executeQuery("SELECT id from Pedido p where p.idCliente =" + id);
+		while (rs.next()) {
+			idPedido = rs.getInt(1);
+			PreparedStatement ps = connection.prepareStatement("DELETE FROM Linea l WHERE l.idPedido = ? ");
+			ps.setInt(1, idPedido);
 			ps.executeUpdate();
 			ps.close();
-			
-			// Ejecuta query de eliminación, actualización o insercioń (DELETE, UPDATE, INSERT)
-			//statement.executeUpdate("INSERT INTO Cliente (nombre, apellido, email, fechaNacimiento, genero) "
-			//						+ "VALUES (" + ", 'Ricciardiello', 'rr0@yelp.com', '1983-04-15', 'M');\n");
+		}
+
+		PreparedStatement ps1 = connection.prepareStatement("DELETE FROM Pedido p WHERE p.idCliente = ? ");
+		ps1.setInt(1, id);
+		ps1.executeUpdate();
+		ps1.close();
+		PreparedStatement ps2 = connection.prepareStatement("DELETE FROM Cliente c WHERE c.id = ?");
+		ps2.setInt(1, id);
+		ps2.executeUpdate();
+		ps2.close();
+
 	}
-	
+
+	public void anniadirPedido(int id, String codigo, String status, int idCliente) throws SQLException {
+		Connection connection = conect();
+		Statement statement = statement(connection);
+		PreparedStatement ps = connection
+				.prepareStatement("INSERT INTO Pedido (id, codigo, status, idCliente) " + " VALUES (?, ?, ?, ?)");
+		ps.setInt(1, id);
+		ps.setString(2, codigo);
+		ps.setString(3, status);
+		ps.setInt(4, idCliente);
+		ps.executeUpdate();
+		ps.close();
+
+	}
+
+	public void anniadirLinea(int id, String codigo, String nombreProducto, int idPedido, int cantidad, int precio)
+			throws SQLException {
+		Connection connection = conect();
+		Statement statement = statement(connection);
+		PreparedStatement ps = connection
+				.prepareStatement("INSERT INTO Linea (id, codigo, nombreProducto, idPedido, cantidad, precio) "
+						+ " VALUES (?, ?, ?, ?,?,?)");
+		ps.setInt(1, id);
+		ps.setString(2, codigo);
+		ps.setString(3, nombreProducto);
+		ps.setInt(4, idPedido);
+		ps.setInt(5, cantidad);
+		ps.setInt(6, precio);
+		ps.executeUpdate();
+		ps.close();
+
+	}
 
 }
